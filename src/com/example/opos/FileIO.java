1eceb14f28d593b276725ff561076c716463a745
LinSinSin <xxice520@hotmail.com>
2015-03-24 20:13:25 +0800

    new FileIO class

---
+++ src/com/example/opos/FileIO.java
@@ -0,0 +1,38 @@
+package com.example.opos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileIO {
+	String readData = "";
+
+	public void writeFile(String fileName) {
+		try {
+			FileWriter fw = new FileWriter("/sdcard/" + fileName, false);
+			BufferedWriter bw = new BufferedWriter(fw); // 將BufferedWeiter與FileWrite物件做連結
+			bw.write("Hello, Sih! Hello, Android!");
+			bw.newLine();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String readFile(String fileName) {
+		try {
+			FileReader fr = new FileReader("/sdcard/" + fileName);
+			BufferedReader br = new BufferedReader(fr);
+			while (br.ready()) {
+				readData += br.readLine() + " ";
+			}
+			fr.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return readData;
+	}
+}
--- src/com/example/opos/MainActivity.java
+++ src/com/example/opos/MainActivity.java
@@ -2,35 +2,44 @@ package com.example.opos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	TextView textView;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		textView = (TextView) findViewById(R.id.textview1);
+		// FileIO io = new FileIO();
+		// io.writeFile();
+		// textView.setText("!" + io.readFile("output.txt"));
+		// Log.i("!", "hi");
+		// textView.setText(read.readFile("ooo.txt") + "22");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
